df6b12a77cd1fe9b2de0c3d05b04db7835a3b5dd
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2023-02-02 16:51:17 +0300

    feat: add timer

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// let seconds = 30;
-// seconds = setInterval((Number(seconds) - 1), 1000);
-// count = setInterval(count, 1000);
+// 1. Создать таймер (один на весь класс (?)) с обратным отсчётом, 
+// обновляемый после 0 сек.
+// 2. Создать случайный arr с men, woman, child.
+// 3. Если man - сравнивать queue-timer. 
+// 4. Если woman - сравнивать queue-ppl. 
+// 5. Если chaild - выбрать случайным образом.
+
+// let seconds = document.getElementsByClassName('queue-timer');
+// let timer = setInterval(seconds - 1), 1000);
 
 
-// function count() {
-//   for (count = Number("timer"); count >= 0; count--) {
-//     }
-//   }
-//   timer = count;
-// }
+function timer() {
+let seconds = document.getElementsByClassName('queue-timer');
+let timerInterval = setInterval(function () {
+  for (i = seconds; i >= 0; i--) {
+    if (i = 0) {
+      timer();
+    }
+    else {
+      return seconds;
+    }
+    }}, 1000);
+  }
 
-var timer = 0;
-var timerInterval;
-var second = document.getElementById('timer');
+  function start() {
+    timer();
+  }
 
-function start() {
-  stop();
-  timerInterval = setInterval(function() {
-  timer += 1/60;
-  secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
-  second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
-  }, 1000/60);
-}
-
-function stop() {
+  function stop() {
   clearInterval(timerInterval);
 }
